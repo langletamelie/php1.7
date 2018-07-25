ce18d2f5c9d13a21b80a429c02b467b7de087023
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 19:15:04 +0200

    php1.7

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP 1.7</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $lastname = 'Lechat';
+    $firstname = 'Lucien';
+    $age = 15;
+    echo 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . ' ans';
+     ?>
+    </p>
+  </body>
+</html>
